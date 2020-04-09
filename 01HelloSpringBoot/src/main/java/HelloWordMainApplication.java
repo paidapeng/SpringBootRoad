4f7a718a0659559dedc3197d0d75da3dce262f7f
liush <liush@gzsendi.cn>
2020-04-09 17:55:11 +0800

    begin

---
+++ 01HelloSpringBoot/src/main/java/HelloWordMainApplication.java
@@ -0,0 +1,13 @@
+package PACKAGE_NAME;
+
+/**
+ *   
+ *  @Title: HelloWordMainApplication
+ *  @Package PACKAGE_NAME
+ *  @Description: SpringBoot 主程序
+ *  @author Liush  
+ *  @date 2020/4/8
+ *  @version V1.0  
+ **/
+public class HelloWordMainApplication {
+}
